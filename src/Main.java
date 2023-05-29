81715f51643082b35831bda8cd79a205825f7411
cristian-simba <davidsimba200@gmail.com>
2023-05-29 17:12:46 -0500

    tercer commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Pelicula spiderman = new Pelicula("Spiderman",1999);
         Pelicula up = new Pelicula("Up",2002);
+        Pelicula fastf = new Pelicula("Rapidos y Furiosos X", 2023);
         spiderman.imprimir();
         up.imprimir();
+        fastf.imprimir();
     }
 }
