89aca39e91fe6dfa91304ca95535c03d55550481
cristian-simba <davidsimba200@gmail.com>
2023-05-29 17:09:42 -0500

    Segunda peli

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Pelicula spiderman = new Pelicula("Spiderman",1999);
+        Pelicula up = new Pelicula("Up",2002);
         spiderman.imprimir();
+        up.imprimir();
     }
 }
