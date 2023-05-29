197f3a3dfdb1e49f4eb86eaf5a077afe3445c2b2
cristian-simba <davidsimba200@gmail.com>
2023-05-29 17:35:58 -0500

    rama 1

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println(spiderman.getNombre());
         System.out.println(spiderman.getFechaEstreno());
         System.out.println("XD");
+        System.out.println("Pa la primera rama");
     }
 }
